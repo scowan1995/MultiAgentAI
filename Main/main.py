d730fe2bcd8975fcd5d875e53c59f97025f459d6
Simone <simofoti@gmail.com>
2019-02-07 00:17:44 +0000

    2-load-all-required-datasests-using-refactored `LoadPreprocess` class

--- Main/main.py
+++ Main/main.py
@@ -5,30 +5,36 @@ import pandas as pd
 sys.path.append("../")
 
 from Configs.configs import statics, configs
-from Preprocessing.preprocessing import Load_Preprocess
+from Preprocessing.preprocessing import LoadPreprocess
 from Model.data_exploration import Data_Exploration
 from Model.logistic_regression import Logistic_Regression
 #from Model.neural_network import
 
+
+def load_all_datasets_and_explore_them(available_datasets):
+    loaded_datasets = {}
+    for current_dataset_name in available_datasets:
+        if configs[current_dataset_name]:
+            loaded_datasets[current_dataset_name] = LoadPreprocess(statics['data'][current_dataset_name],
+                                                                   use_numerical_labels=True)
+            if configs['data_exploration']:
+                data_exploration = DataExploration(loaded_datasets[current_dataset_name])
+    return loaded_datasets
 
 if __name__ == "__main__":
 
 
     ## DATA_________________________________________________________________________
 
-    # data object contains "features", "targets", "features_num" and "mapping"
-    data = Load_Preprocess()
+    datasets_names = ['mock', 'train', 'val', 'test']
+    datasets = load_all_datasets_and_explore_them(datasets_names)
 
 
     ## MODEL________________________________________________________________________
 
-    if configs['data_exploration']:
-
-        data_exploration = Data_Exploration(data.mock_data)
-
     if configs['logistic_regression']:
 
-        logistic_regression = Logistic_Regression(data.mock_features_num, data.mock_targets)
+        logistic_regression = Logistic_Regression(datasets['mock'].data_features, datasets['mock'].data_targets)
         print(logistic_regression.score)
 
 
