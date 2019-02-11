2c7ca31a8a628efd7501a76037b65fd3ead9162a
Sean <sean.cowan.18@ucl.ac.uk>
2019-02-11 16:30:32 +0000

    Added the ECPC per feature
    This code plots the expected click per feature
    
    So it calculates the eexpected cost per click given feature x = value y
    Be aware:
    currently if any of value y are not numbers it returns 0 ecpc

--- ecpc.py
+++ ecpc.py
@@ -1,22 +1,95 @@
 from data_loader import DataLoader
+import os
+import pandas as pd
+import matplotlib.pylab as plt
+
+from Configs.configs import statics, configs
 
 
 class ECPC:
     # Class for calculating the expected cost per click
 
     def __init__(self):
-        dl = DataLoader(load_small=False)
-        self.train, self.test = dl.get_train_val(validation_size=0)
+        path = os.path.abspath(__file__ + "/../") + "/Data/train.csv"
+        data_pandas = pd.read_csv(path, na_values=["Na", "null"]).fillna(0)
+        self.data = data_pandas
+        self.data_targets = self.data[configs["target"]]
+        self.plottable_items = []
+        self.feature_list = [
+            "weekday",
+            "hour",
+            "useragent",
+            "region",
+            "city",
+            "adexchange",
+            "creative",
+            "advertiser",
+            "domain",
+        ]
 
-    def calculate(self):
+        # not keeping 'userid' or 'IP' because thats just expected clcik per user
+        #  'url', 'slotid', 'slotwidth', 'slotheight',
+        #  'slotvisibility', 'slotformat', 'slotprice', 'keypage',
+        #   'usertag'], # select the data features
+
+    def calculate(self, data):
         """
         Calculates the expected cost per click
         sum(cost) / num_clicks
         """
         numerator = self.train.loc[self.train["click"] == 1, "payprice"].sum()
         denominator = self.train.loc[self.train["click"] == 1, "click"].sum()
-        print(numerator/denominator)
+        return numerator / denominator
+
+    def getPossibleValues(self, feature):
+        try:
+            return sorted(self.data[feature].unique())
+        except:
+            print("Error, returning empty")
+            print("feature didn't work:", feature)
+            return []
+
+    def calc_ecpc_given_feature(self, feature, value):
+        numerator = self.data.loc[
+            (self.data["click"] == 1) & (self.data[feature] == value),
+            "payprice"
+        ].sum()
+        denominator = self.data.loc[
+            (self.data["click"]) == 1 & (self.data[feature] == value),
+            "click"
+        ].sum()
+        if denominator == 0 or numerator == 0:
+            return 0
+        return numerator / denominator
+
+    def calc_per_feature(self):
+        for feat in self.feature_list:
+            expected_price_per_click_given_feature = []
+            possible_values = self.getPossibleValues(feat)
+            possible_values = list(
+                map(
+                    lambda x: (x, self.calc_ecpc_given_feature(feat, x)),
+                    possible_values,
+                )
+            )
+            self.plottable_items.append((feat, possible_values))
+
+    def plot_ecpc_features(self):
+        """
+        plot the plottable items
+        """
+        for item in self.plottable_items:
+            x_ticks = list(map(lambda x: x[0], item[1]))
+            plt.plot(range(len(x_ticks)), list(map(lambda x: x[1], item[1])))
+            rotation = 'horizontal'
+            if type(x_ticks[0]) == str:
+                rotation = 'vertical'
+            plt.xticks(list(range(len(x_ticks))), x_ticks, rotation=rotation)
+            plt.title(item[0])
+            plt.show()
+
 
 if __name__ == "__main__":
     ecpc = ECPC()
-    ecpc.calculate()
+    ecpc.calc_per_feature()
+    ecpc.plot_ecpc_features()
