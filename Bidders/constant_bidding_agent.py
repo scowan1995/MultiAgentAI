e86ab69e6dd9dcc61194693684aeda1741df3a25
Simone <simofoti@gmail.com>
2019-02-13 02:04:53 +0000

    4-remove useless `constant_bid_value`

--- Bidders/constant_bidding_agent.py
+++ Bidders/constant_bidding_agent.py
@@ -4,18 +4,16 @@ from .basic_bidding_agent import BasicBiddingAgent
 class ConstantBiddingAgent(BasicBiddingAgent):
 
     def __init__(self, training_set, initial_budget):
-        self.constant_bid_value = 0
         super().__init__(training_set, initial_budget)
-
 
     def _train(self, training_set):
         """TODO: explore data (use class DataExploration) to determine reasonable constant bids"""
-        self.constant_bid_value = 10
+        self._bid_value = 1000
         pass
 
     def _bidding_function(self, utility=None, cost=None):
         """Deploy the learned bidding model"""
-        return self.constant_bid_value
+        return self._bid_value
 
 
 
