df4b55cf41f5407d3def71d2bc90903b55f6ba28
Simone <simofoti@gmail.com>
2019-02-21 15:35:32 +0000

    Add a draft of NNBiddingAgent

--- Bidders/neural_network_bidding_agent.py
+++ Bidders/neural_network_bidding_agent.py
@@ -1,22 +1,16 @@
 from .basic_bidding_agent import BasicBiddingAgent
 
 
-class ConstantBiddingAgent(BasicBiddingAgent):
-
+class NeuralNetworkBiddingAgent(BasicBiddingAgent):
     def __init__(self, training_set, initial_budget):
         super().__init__(training_set, initial_budget)
 
     def _train(self, training_set):
-        """TODO: explore data (use class DataExploration) to determine reasonable constant bids"""
-        self._bid_value = 277
+        """TODO: explore data (use class DataExploration) to determine a reasonable range"""
+        # training_set.data_feature
         pass
 
     def _bidding_function(self, utility=None, cost=None):
         """Deploy the learned bidding model"""
-        return self._bid_value
-
-    def set_const_bid_value(self, bid):
-        self._bid_value = bid
-
-
-
+        # write fwd step of nn
+        pass
